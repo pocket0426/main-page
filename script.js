46c633cb281fe90e8c6312e7c5cafbba03b26cd9
pocket0426 <115338938+pocket0426@users.noreply.github.com>
2023-10-04 16:39:20 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("mousemove", function(event) {
+    var circle = document.querySelector(".circle");
+    circle.style.left = event.clientX + "px";
+    circle.style.top = event.clientY + "px";
+ });
